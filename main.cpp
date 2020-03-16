a22fcd67511a06fe7a39350d1949f3349c8b6c51
simon-gonand <simon.gonand@etu.univ-amu.fr>
2020-03-16 10:33:37 +0000

    remove useless cube + correct blue light position

--- main.cpp
+++ main.cpp
@@ -35,14 +35,14 @@ rt3d::lightStruct lightBlue = {
 	{0.0f, 0.0f, 1.0f, 1.0f}, // specular
 	{-6.0f, 3.0f, 14.0f, 1.0f}, // position
 };
-glm::vec4 lightBluePos(6.0f, 5.0f, 14.0f, 1.0f);
+glm::vec4 lightBluePos(6.0f, 3.0f, 14.0f, 1.0f);
 
 // Yellow light
 rt3d::lightStruct lightYellow = {
 	{0.3f, 0.3f, 0.3f, 1.0f}, // ambient
 	{1.0f, 1.0f, 0.0f, 1.0f}, // diffuse
 	{1.0f, 1.0f, 0.0f, 1.0f}, // specular
-	{6.0f, 5.0f, 14.0f, 1.0f}, // position
+	{6.0f, 3.0f, 14.0f, 1.0f}, // position
 };
 glm::vec4 lightYellowPos(-6.0f, 3.0f, 14.0f, 1.0f);
 
@@ -312,15 +312,6 @@ void draw(SDL_Window* window) {
 	if (rotationBluePlane.x != 0 || rotationBluePlane.z != 0 )
 		drawStack.top() = glm::rotate(drawStack.top(), float(rotationBlueAngle * DEG_TO_RADIAN), rotationBluePlane);
 	drawStack.top() = glm::scale(drawStack.top(), glm::vec3(3.0f, 0.2f, 5.0f));
-	rt3d::setUniformMatrix4fv(shaderProgram, "model", glm::value_ptr(drawStack.top()));
-	rt3d::setMaterial(shaderProgram, materialMap);
-	rt3d::drawIndexedMesh(meshObjects, meshIndexCount, GL_TRIANGLES);
-	drawStack.pop();
-
-	// blue light position
-	drawStack.push(drawStack.top());
-	drawStack.top() = glm::translate(drawStack.top(), glm::vec3(lightBluePos[0], lightBluePos[1], lightBluePos[2]));
-	drawStack.top() = glm::scale(drawStack.top(), glm::vec3(1.0f, 1.0f, 1.0f));
 	rt3d::setUniformMatrix4fv(shaderProgram, "model", glm::value_ptr(drawStack.top()));
 	rt3d::setMaterial(shaderProgram, materialMap);
 	rt3d::drawIndexedMesh(meshObjects, meshIndexCount, GL_TRIANGLES);
